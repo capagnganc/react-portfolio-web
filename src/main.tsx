fc65f9552863a0faffef27d9c584d17ebf253815
DaikuSumairu <daikusumairu84@gmail.com>
2024-12-03 19:26:48 +0800

    change root

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
+
+import Dashboard from './Dashboard.tsx'
+import Router from './Router.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
   </StrictMode>,
 )
