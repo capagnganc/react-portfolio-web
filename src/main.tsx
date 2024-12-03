0c28fcf9b35bd3288a7027cdfe5019044b14ddc5
DaikuSumairu <daikusumairu84@gmail.com>
2024-12-03 19:27:26 +0800

    change root

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'             // creating routing function
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
